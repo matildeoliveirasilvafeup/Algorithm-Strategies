cc75215f8f1146673dcf4e8d2b034125b3497628
Tiago Almeida <up202303450@up.pt>
2024-11-22 14:59:13 +0000

    Movimento Base

--- src/main/java/Jogo.java
+++ src/main/java/Jogo.java
@@ -1,2 +1,42 @@
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
 public class Jogo {
+    public static void main(String[] args){
+        try{
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+            Screen screen = new TerminalScreen(terminalFactory.createTerminal());
+            screen.startScreen();
+
+            TerminalSize terminalSize = screen.getTerminalSize();
+            Personagem player = new Personagem(terminalSize.getColumns()/2, terminalSize.getRows()/2,'@' );
+
+            boolean running = true;
+            while(running){
+                screen.clear();
+                player.draw(screen);
+                screen.refresh();
+                KeyStroke keyStroke = screen.pollInput();
+                if(keyStroke != null){
+                    switch(keyStroke.getKeyType()){
+                        case ArrowUp -> player.move(0,-1,terminalSize);
+                        case ArrowDown -> player.move(0,1,terminalSize);
+                        case ArrowLeft -> player.move(-1,0,terminalSize);
+                        case ArrowRight -> player.move(1,0,terminalSize);
+                        case Escape, Character -> running = false;
+                    }
+                }
+                Thread.sleep(25);
+            }
+            screen.startScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Personagem.java
+++ src/main/java/Personagem.java
@@ -1,2 +1,23 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.screen.Screen;
+
 public class Personagem {
+    private int x;
+    private int y;
+    private final char symbol;
+
+    public Personagem(int startX, int startY, char symbol){
+        this.x = startX;
+        this.y = startY;
+        this.symbol = symbol;
+    }
+    public void draw(Screen screen){
+        screen.setCharacter(x,y,new TextCharacter(symbol, TextColor.ANSI.GREEN, TextColor.ANSI.BLACK));
+    }
+    public void move(int dx, int dy, TerminalSize terminalSize){
+        x = Math.max(0, Math.min(x+dx, terminalSize.getColumns()-1));
+        y = Math.max(0, Math.min(y+dy, terminalSize.getRows()-1));
+    }
 }
